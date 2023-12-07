86dcb43c8d21c262d72d4046686e20f49fe4e41e
master <alya26271@gmail.com>
2023-12-07 05:16:21 +0200

    GetSpecialNames, SortedNames, GetNumAndSort, EndlessStream

--- src/SortedNames.java
+++ src/SortedNames.java
@@ -7,7 +7,7 @@ public class SortedNames {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Anton", "Denis", "Igor", "Maksim", "Petro", "Ivan", "Sasha", "Oleg");
 
-        namesReverse(names).forEach(System.out::println);
+        namesReverse(names).stream().map(String::toUpperCase).forEach(System.out::println);
     }
 
     public static List<String> namesReverse(List<String> names) {
